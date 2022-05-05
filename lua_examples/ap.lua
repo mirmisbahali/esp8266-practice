0908370a2b88fcc0e996cfa195cb6eb52c6c038f
Mir Misbah Ali <mirmisbahali@hotmail.com>
2022-05-05 23:26:35 +0530

    controlling the builit in led using web page

--- lua_examples/ap.lua
+++ lua_examples/ap.lua
@@ -1,16 +1,18 @@
 wifi.setmode(wifi.SOFTAP)
 wifi.ap.config({ ssid = "test",  auth = wifi.OPEN})
 
+
 srv = net.createServer(net.TCP)
 
-srv:listen(80, function(conn)
-    print("connection made")
-    print(wifi.ap.getip())
-    conn:on('receive', function(client, request)
-    print("connection receive")
-    print("request:\t" .. request)
-    client:send('<h1>hellow world</h1>')
+print("Visit http://" .. wifi.ap.getip()) -- Get the IP address
 
+srv:listen(80, function(conn)
+    
+    print("connection made")
+    
+    conn:on('receive', function(client, request)
+    client:send('<h1>hello world</h1>')
     end) 
+
     conn:on("sent", function(c) c:close() end)
 end)
--- lua_examples/led/init_test.lua
+++ lua_examples/led/init_test.lua
@@ -1,6 +1,31 @@
-function redirect()
-    print("connected")
+wifi.setmode(wifi.SOFTAP)
+wifi.ap.config({ssid = "esp", pwd = "123456789", auth = wifi.WPA2_PSK})
 
+ledPin = 0
+gpio.mode(ledPin, gpio.OUTPUT)
+
+function control(res)
+    print(res)
+    
+    if res == 'ON' then gpio.write(ledPin, gpio.LOW)
+    else gpio.write(ledPin, gpio.HIGH)
+    end
+
+    return
 end
 
-enduser_setup.start(redirect)
+srv = net.createServer(net.TCP)
+
+srv:listen(80, function(conn) 
+
+    conn:on('receive', function(client, request) 
+
+        find = {string.find(request, 'ledState=')}
+        if #find ~= 0 then control(string.sub(request, find[2] + 1)) end
+
+        client:send('<!DOCTYPE html><html lang="en"><head> <meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>Document</title></head><body> <form action="" method="POST"> <input name="ledState" type="submit" value="OFF"> <input name="ledState" type="submit" value="ON"> </form></body></html>')
+
+
+        conn:on("sent", function(c) c:close() end)
+    end)
+end)

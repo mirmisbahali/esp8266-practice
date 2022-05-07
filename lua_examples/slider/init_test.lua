a7827193804439a69dcd7fd5d1b63d0cfaa18f7f
Mir Misbah Ali <mirmisbahali@hotmail.com>
2022-05-07 18:32:24 +0530

    mearm servos working

--- lua_examples/slider/init_test.lua
+++ lua_examples/slider/init_test.lua
@@ -7,13 +7,37 @@ function disconnected()
     print("Nodemcu disconnected")
 end
 
+function mysplit (inputstr, sep)
+  if sep == nil then
+          sep = "%s"
+  end
+  local t={}
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+          table.insert(t, str)
+  end
+  return t
+end
+
 wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, connected)
 wifi.eventmon.register(wifi.eventmon.STA_DISCONNECTED, disconnected)
 wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function() print("IP: " .. wifi.sta.getip()) end)
 wifi.setmode(wifi.STATION)
 wifi.sta.config({ssid = "esp-guest", pwd = "12345678!"})
 
+baseServoPin = 1
+leftServoPin = 2
+rightServoPin = 3
+clawServoPin = 4
 
+pwm.setup(clawServoPin, 50, 20)
+pwm.setup(leftServoPin, 50, 100)
+pwm.setup(rightServoPin, 50, 100)
+pwm.setup(baseServoPin, 50, 40)
+
+pwm.start(clawServoPin)
+pwm.start(leftServoPin)
+pwm.start(rightServoPin)
+pwm.start(baseServoPin)
 
 srv = net.createServer(net.TCP)
 
@@ -26,86 +50,119 @@ srv:listen(80, function(conn)
 
         
         find = {string.find(request, 'slider')}
+
         
-        if #find ~= 0 then            
+        if #find ~= 0 then
             args = string.sub(request, find[2]+2, #request)
             
             local servo = {}
-            if (args ~= nil) then
+            
+            sl = mysplit(args, "&")
+            servo.name = mysplit(sl[1], "=")[2]
+            
+            if (sl[2] ~= nil) then
                 local n = 0
-                for k, v in string.gmatch(args, "(%w+)=(%w+)") do
-                    if string.match(k, "Servo") then
-                        servo.name = k
+                for k, v in string.gmatch(sl[2], "(%w+)=(%w+)") do
+                    if string.match(k, "value") then
                         servo.pos = tonumber(v)
                     end
                 end
             end
 
-            local clawServoPin = 1
-            local leftServoPin = 2
-            local rightServoPin = 3
-            local baseServoPin = 4
-
-            pwm.setup(clawServoPin, 50, 20)
-            pwm.setup(leftServoPin, 50, 20)
-            pwm.setup(rightServoPin, 50, 20)
-            pwm.setup(baseServoPin, 50, 20)
-
-            pwm.start(clawServoPin)
-            pwm.start(leftServoPin)
-            pwm.start(rightServoPin)
-            pwm.start(baseServoPin)
             
-            local 
-            function setServoPos(pin, pos) 
+            
+            function setServoPos(pin, pos)
+                print("servoPin = " .. pin) 
+                print("servoPos = " .. pos)
                 dc = ((100/180)*pos) + 20
                 print("Duty Cycle = " .. dc)
                 pwm.setduty(pin, dc)
                 return
             end
 
-            if servo.name == "clawServo" then
+            if (servo.name == "clawServo") then
                 setServoPos(clawServoPin, servo.pos)
-            else if servo.name == "leftServo" then
+            elseif (servo.name == "leftServo") then
                 setServoPos(leftServoPin, servo.pos)
-            else if servo.name == "rightServo" then
-                setServoPos(rightServoPin, servo.pin)
-            else if servo.name = "baseServo" then
-                setServoPos(baseServoPin, servo.pin)
+            elseif (servo.name == "rightServo") then
+                setServoPos(rightServoPin, servo.pos)
+            elseif (servo.name == "baseServo") then
+                setServoPos(baseServoPin, servo.pos)
+            else
+              print("No servo changed")
+            end
 
-
-            
-            
         end
         
         client:send([[
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Slider</title>
-            </head>
-            <body>
-                <input onchange="slider(this)" type="range" name="clawServo" id="clawServo" min="0" max="180">
-                <input onchange="slider(this)" type="range" name="leftServo" id="leftServo" min="0" max="180">
-                <input onchange="slider(this)" type="range" name="rightSerervo" id="rightSerervo" min="0" max="180">
-                <input onchange="slider(this)" type="range" name="baseServo" id="baseServo" min="0" max="180">
-                <script>
-                function slider(el) {
-                    console.log(`Name: ${el.name}`)
-                    console.log(`Value: ${el.value}`)
-                    const xhttp = new XMLHttpRequest();
-                    xhttp.onload = function() {
-                        console.log(el);
-                    }
-                    xhttp.open("GET", `/slider?${el.name}=${el.value}`, true);
-                    xhttp.send();
+          <!DOCTYPE html>
+          <html lang="en">
+          
+          <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Slider</title>
+            <style>
+              * {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+              }
+          
+              body {
+                width: 100vw;
+                height: 100vh;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+              }
+              .form-group {
+                padding: 1.2rem 0;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+              }
+          
+              .form-group label {
+                padding: 0 0.5rem;
+              }
+            </style>
+          </head>
+          
+          <body>
+            <div class="form-group">
+              <label for="clawServo">Claw</label>
+              <input onchange="slider(this)" type="range" name="clawServo" id="clawServo" min="0" max="180">
+            </div>
+            <div class="form-group">
+              <label for="leftServo">Left</label>
+              <input onchange="slider(this)" type="range" name="leftServo" id="leftServo" min="0" max="180">
+            </div>
+            <div class="form-group">
+              <label for="rightServo">Right</label>
+              <input onchange="slider(this)" type="range" name="rightServo" id="rightServo" min="0" max="180">
+            </div>
+            <div class="form-group">
+              <label for="baseServo">Base</label>
+              <input onchange="slider(this)" type="range" name="baseServo" id="baseServo" min="0" max="180">
+            </div>
+            <script>
+              function slider(el) {
+                console.log(`Name: ${el.name}`)
+                console.log(`Value: ${el.value}`)
+                const xhttp = new XMLHttpRequest();
+                xhttp.onload = function () {
+                  console.log(el);
                 }
-                </script>
-            </body>
-            </html>
+                xhttp.open("GET", `/slider?name=${el.name}&value=${el.value}`, true);
+                xhttp.send();
+              }
+            </script>
+          </body>
+          
+          </html>
         ]])
 
 
